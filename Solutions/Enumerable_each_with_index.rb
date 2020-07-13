b04cfed70a51915b24ffbb9118a21206ea7a699c
Aman Upadhyay <55270177+xBadshahx@users.noreply.github.com>
2020-07-13 17:00:42 +0530

    Update Enumerable_each_with_index.rb

--- Solutions/Enumerable_each_with_index.rb
+++ Solutions/Enumerable_each_with_index.rb
@@ -1,3 +1,9 @@
+#/*
+# * 
+# * Aman Upadhyay
+# * Amity University Kolkata
+# * 
+#*/
 def skip_animals(animals, skip)
     arr=Array.new
     animals.each_with_index {|it,ind| arr.push("#{ind}:"+it) unless ind < skip}
