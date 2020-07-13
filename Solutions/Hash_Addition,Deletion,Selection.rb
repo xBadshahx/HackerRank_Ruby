fe1b06ab6d7724a7bfd0a1e5dc22bc76b0c8762d
Aman Upadhyay <55270177+xBadshahx@users.noreply.github.com>
2020-07-13 17:02:36 +0530

    Update Hash_Addition,Deletion,Selection.rb

--- Solutions/Hash_Addition,Deletion,Selection.rb
+++ Solutions/Hash_Addition,Deletion,Selection.rb
@@ -1,3 +1,9 @@
+#/*
+# * 
+# * Aman Upadhyay
+# * Amity University Kolkata
+# * 
+#*/
 hackerrank.store(543121, 100)
 hackerrank.keep_if {|key, value| key.is_a? Integer}
 hackerrank.delete_if {|key, value| key&1 == 0}
